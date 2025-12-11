f3036f1b68b3501360b42fd29fe850ceb958f5ac
david1768 <davidsplaystation1011@gmail.com>
2025-12-11 08:54:01 -0600

    Update 4_problem_set_1.py

--- 4_problem_set_1.py
+++ 4_problem_set_1.py
@@ -8,13 +8,19 @@
 
 # Write a program that prints the numbers from **1 to 10**, each on a new line.
 
-
+list1to10 = list(range(1,11))
+for number in list1to10:
+    print(number)
 
 # ### **Problem 2: Sum of Numbers
 
 # Ask the user for a number **n**, then calculate and display the **sum of all numbers from 1 to n**.
 
-
+n = int(input("Enter a number: "))
+total_sum = 0
+for number in range(1, n + 1):
+  total_sum += number
+print("The sum of numbers from 1 to", n, "is", total_sum)
 
 # ### **Problem 3: Factorial Calculator
 
@@ -22,6 +28,20 @@
 
 # *(Example: factorial of 5 is 120)
 
+for i in range(10):
+  print(i)
+
+def factorial(n):
+
+  factorial=1
+
+  for i in range(n):
+    factorial*=i+1
+
+  return factorial
+
+
+print(factorial(5))
 
 # ### **Problem 4: Count Vowels**
 
@@ -32,8 +52,18 @@
 
 # Ask the user for a number **n**, then print all **even numbers** from 2 up to n.
 
+n = int(input("enter a number: ")
+print("even numbers from 2 to", n, ":")
+for number in range(2, n + 1, 2):
+        print(number)
 
-
+list_even_numbers = list(range(1, 45))
+for number in list_even_numbers:
+# if number is even, print it
+  if number % 2 == 0:
+    print(even number", number)
+else:
+  print("odd number, skipping", number)
 # ### **Problem 6: Reverse a String**
 
 # Ask the user for a string, then print the string **backwards**.
